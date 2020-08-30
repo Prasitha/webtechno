cf5f0d4d1f7c6804e2aea5fbf3723c18211ec8bf
Prasitha <68117578+Prasitha@users.noreply.github.com>
2020-08-30 14:04:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.alert("Kindly fill the feedback form!!");
+
+var user = window.prompt("Enter your name:");
+
+function myFunction() {
+    alert("Are you sure you want to submit the form?");
+  }
